929a6db09b41887187726cbba585dcd7590ccc27
hien1206 <124993130+hien1206@users.noreply.github.com>
2023-03-26 19:55:29 +0700

    create project

---
+++ include/footer.php
@@ -0,0 +1,62 @@
+
+	<!-- footer -->
+	<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi</h2>
+				<p class="footer-main mb-4">
+					Nếu bạn đang cân nhắc một chiếc máy tính xách tay mới, đang tìm kiếm một chiếc smartphone tốt, hiện đại,
+					chúng tôi sẽ giúp bạn dễ dàng tìm thấy chính xác những gì bạn cần với mức giá bạn có thể mua được.
+					Chúng tôi cung cập nhật giá thấp hàng ngày..</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí vận chuyển</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Vận chuyển nhanh</h3>
+								<p>Toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Tin cậy</h3>
+								<p>Sự đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		<!-- //footer third section -->
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© RUOK Store | Design by 
+				<a href="#"> Huỳnh Trọng Hiển</a>
+			</p>
+		</div>
+	</div>
+	<!-- //copyright -->
